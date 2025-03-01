618c77dc7f2e937397ac7935b369b20c4361765a
ericshiyx <shiyx2000@gmail.com>
2025-02-28 17:50:43 -0700

    Update CORS settings for Netlify domain

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',  // Local development
-    'https://candycompare.netlify.app/'  // Your Netlify domain
+    'https://candycompare.netlify.app'  // Your Netlify domain
   ],
   credentials: true
 }));
